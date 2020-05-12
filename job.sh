502756d867e5b3852d6839d279da0787bf8b2942
Mohammad Hadi Salari <msalari@cedar1.cedar.computecanada.ca>
2020-05-12 15:49:07 -0700

    Add slurm job script

---
+++ job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mail-user=salari.m1375@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-mori_gpu
+#SBATCH --job-name=cifar100
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=03:00:00
+#SBATCH --mem=0
+#SBATCH --gres=gpu:p100l:1
+#SBATCH --cpus-per-task=3
+
+cd $SLURM_TMPDIR
+cp -r ~/scratch/pytorch-cifar100 .
+cd pytorch-cifar100
+
+module load python/3.7
+virtualenv --no-download venv
+source venv/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+
+
+python train -net resnet18
+
